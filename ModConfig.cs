b118cfe1cf505ae7acd805b06d6e0e3460bd430d
Danny Pearmain <thecosmicslug@gmail.com>
2023-08-31 15:41:03 +0100

    Initial Commit

---
+++ ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+
+namespace ExpandedFridge
+{
+    /// <summary>
+    /// Stores options for the Manager.
+    /// </summary>
+    public class ModConfig
+    {
+        public bool HideMiniFridges { get; set; } = true;
+        public SButton NextFridgeTabButton { get; set; } = SButton.RightTrigger;
+        public SButton LastFridgeTabButton { get; set; } = SButton.LeftTrigger;
+    }
+}
---
+++ build.sh
@@ -0,0 +1,3 @@
+echo off
+dotnet build .
+rm -rf obj
