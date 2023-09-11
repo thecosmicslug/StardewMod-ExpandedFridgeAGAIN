0ab42443da303a76ff7deb2b662b2266688ae4e5
Danny Pearmain <thecosmicslug@gmail.com>
2023-09-11 18:25:38 +0100

    warn about 'BetterChests'

--- ModEntry.cs
+++ ModEntry.cs
@@ -10,11 +10,9 @@ namespace ExpandedFridge
     public class ModEntry : Mod
     {
         private static ModEntry _instance = null;
-
         public ModConfig Config { get; private set; }
         public FridgeManager FridgeManager { get; private set; }
     
-
         //* Setup instance and load our first event-hooks
         public override void Entry(IModHelper helper)
         {
@@ -38,12 +36,12 @@ namespace ExpandedFridge
                 api.Register(ModManifest, () => Config = new ModConfig(), () => Helper.WriteConfig(Config));
 
                 //* Our Key-binds
-                api.AddKeybind(ModManifest, () => Config.NextFridgeTabButton, (SButton val) => Config.NextFridgeTabButton = val, () => Helper.Translation.Get("Config.NextFridgeTabButton"), () => Helper.Translation.Get("Config.NextFridgeTabButtonDesc"));
-                api.AddKeybind(ModManifest, () => Config.LastFridgeTabButton, (SButton val) => Config.LastFridgeTabButton = val, () => Helper.Translation.Get("Config.LastFridgeTabButton"), () => Helper.Translation.Get("Config.LastFridgeTabButtonDesc"));
+                api.AddKeybind(ModManifest, () => Config.NextFridgeTabButton, (SButton val) => Config.NextFridgeTabButton = val, () => Helper.Translation.Get("Config.NextFridgeTabButton"), () => Helper.Translation.Get("Config.NextFridgeTabButtonDesc"),"NextFridgeTabButton");
+                api.AddKeybind(ModManifest, () => Config.LastFridgeTabButton, (SButton val) => Config.LastFridgeTabButton = val, () => Helper.Translation.Get("Config.LastFridgeTabButton"), () => Helper.Translation.Get("Config.LastFridgeTabButtonDesc"),"LastFridgeTabButton");
 
                 //* Our Options
-                api.AddBoolOption(ModManifest,() => Config.ShowDebugMessages, (bool val) => Config.ShowDebugMessages = val, ()=> Helper.Translation.Get("Config.ShowDebugMessages"), () => Helper.Translation.Get("Config.ShowDebugMessagesDesc"),"ShowDebugMessages");
-                api.AddBoolOption(ModManifest,() => Config.HideMiniFridges, (bool val) => Config.HideMiniFridges = val, ()  => Helper.Translation.Get("Config.HideMiniFridges"), () => Helper.Translation.Get("Config.HideMiniFridgesDesc"),"HideMiniFridges");
+                api.AddBoolOption(ModManifest,() => Config.ShowDebugMessages, (bool val) => Config.ShowDebugMessages = val, ()=> Helper.Translation.Get("Config.ShowDebugMessages" + "."), () => Helper.Translation.Get("Config.ShowDebugMessagesDesc"),"ShowDebugMessages");
+                api.AddBoolOption(ModManifest,() => Config.HideMiniFridges, (bool val) => Config.HideMiniFridges = val, ()  => Helper.Translation.Get("Config.HideMiniFridges") + ".", () => Helper.Translation.Get("Config.HideMiniFridgesDesc"),"HideMiniFridges");
 
                 //* Detect changes mid-game.
                 api.OnFieldChanged(ModManifest,onFieldChanged);
@@ -54,20 +52,29 @@ namespace ExpandedFridge
                 DebugLog(Helper.Translation.Get("Debug.GenericModConfigMenuMissing"), LogLevel.Info);
             }
 
+            //* Check for incompatible mods.
+            //TODO: Check for more incompatible mods.
+            bool BetterChestsLoaded = Helper.ModRegistry.IsLoaded("furyx639.BetterChests");
+            if (BetterChestsLoaded){
+                DebugLog(Helper.Translation.Get("Debug.BetterChestsDetected"), LogLevel.Warn);
+            }    
+
             //* Print options to the log
             if (Config.ShowDebugMessages){
-                DebugLog(Helper.Translation.Get("Debug.DebugEnabled"), LogLevel.Info);
+                DebugLog(Helper.Translation.Get("Debug.OptionEnabledRuntime", new { option = Helper.Translation.Get("Config.ShowDebugMessages") }), LogLevel.Info);
             }else{
-                DebugLog(Helper.Translation.Get("Debug.DebugDisabled"), LogLevel.Info);
+                DebugLog(Helper.Translation.Get("Debug.OptionDisabledRuntime", new { option = Helper.Translation.Get("Config.ShowDebugMessages") }), LogLevel.Info);
             }
-
             if (Config.HideMiniFridges){
-                DebugLog(Helper.Translation.Get("Debug.HideMiniFridgeEnabled"), LogLevel.Info);
+                DebugLog(Helper.Translation.Get("Debug.OptionEnabledRuntime", new { option = Helper.Translation.Get("Config.HideMiniFridges") }), LogLevel.Info);
             }else{
-                DebugLog(Helper.Translation.Get("Debug.HideMiniFridgeDisabled"), LogLevel.Info);
+                DebugLog(Helper.Translation.Get("Debug.OptionDisabledRuntime", new { option = Helper.Translation.Get("Config.HideMiniFridges") }), LogLevel.Info);
             }
 
-            //* Start FridgeManager before the start of the first day.
+            DebugLog(Helper.Translation.Get("Debug.KeySetTo", new { option = "NextFridgeTabButton" }) + Config.NextFridgeTabButton.ToString() + ".", LogLevel.Info);
+            DebugLog(Helper.Translation.Get("Debug.KeySetTo", new { option = "LastFridgeTabButton" }) + Config.LastFridgeTabButton.ToString() + ".", LogLevel.Info);
+
+            //* Start FridgeManager once we are all setup & before the start of the first day.
             FridgeManager = new FridgeManager(_instance);
 
         }
@@ -88,6 +95,12 @@ namespace ExpandedFridge
                 }else{
                     DebugLog(Helper.Translation.Get("Debug.OptionDisabledRuntime", new { option = str }), LogLevel.Info);
                 }
+            }
+            else if(str == "NextFridgeTabButton"){
+                DebugLog(Helper.Translation.Get("Debug.KeySetTo", new { option = str }) + obj.ToString() + ".", LogLevel.Info);
+            }
+            else if(str == "LastFridgeTabButton"){
+                DebugLog(Helper.Translation.Get("Debug.KeySetTo", new { option = str }) + obj.ToString() + ".", LogLevel.Info);
             }
         }
 
